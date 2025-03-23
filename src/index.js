4b9a8cbc47f8a61d7291bcbadab877fd3b9049f6
Yurii Pochapskiy <pochapskiy1983@gmail.com>
2025-03-23 08:55:21 +0200

    add: js-hw-18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import Post from './post';
 import './css/style.scss';
-import logo from './assets/icon-square-big.png';
-import fav from '../favicon.png';
-import settings from './assets/data.json'
 
-const post = new Post('Webpack Post Title!!!')
 
-console.log('Post to string:', post.toString())
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
 
 
+
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   mode: 'development',
@@ -12,22 +19,55 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].bundle.js',
+    filename: isProd ? '[name].bundle.[contenthash].js' : '[name].js',
   },
+  target: 'web',
+
+  devServer: {
+    port: 4200,
+    hot: false
+  },
+  devtool: isProd ? false : 'source-map',
   plugins: [
     new HTMLWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/index.html'),
-      title: 'My super Project - Main!',
+      title: 'My super Project - Home!',
       chunks: ['main', 'stats']
     }),
-    new CleanWebpackPlugin()
+    new HTMLWebpackPlugin({
+      filename: 'main.html',
+      template: path.resolve(__dirname, 'src/main.html'),
+      title: 'Main!',
+      chunks: ['main', 'stats']
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.png'),
+          to: path.resolve(__dirname, 'dist'),
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: isProd ? '[name].[contenthash].min.css' : '[name].css'
+    }),
+    new EslintWebpackPlugin({
+      extensions: ['js'],
+      fix: true,
+      configType: 'eslinterc'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader'
+        ],
       },
       {
         test: /\.(png|jpg|jpeg|svg|gif|webp)$/,
@@ -36,7 +76,27 @@ module.exports = {
       {
         test: /\.(ttf|woff|woff2|eot)$/,
         type: 'asset/resource'
-      }
+      },
+      {
+        test: /\.js$/, // Відповідає усім .js файлам
+        exclude: /node_modules/, // Виключає папку node_modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'] // Використовує preset-env для транспіляції сучасного JS
+          }
+        }
+      },
+
     ],
+
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimizer: [
+      new TerserPlugin()
+    ]
   }
 };
